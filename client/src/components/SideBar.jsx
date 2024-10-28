3fc8ab2a2ed84fecfb4f4792af63dddb07fb5226
dishant <aryadishant414@gmail.com>
2024-10-28 10:32:53 +0530

    Sidebar done

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,7 +1,29 @@
+import Drawer from '@mui/material/Drawer';
+import SideBarContent from './SideBarContent';
 
-const SideBar = () => {
+const SideBar = ({openDrawer}) => {
   return (
-    <div>Hello from SideBar</div>
+    <Drawer
+      anchor='left'
+      open={openDrawer}
+      hideBackdrop={true}
+      ModalProps={{
+        keepMounted: true
+      }}
+      variant='persistent'
+      sx={{
+        '& .MuiDrawer-paper': {
+          marginTop: '64px',
+          width: 250,
+          background: '#F5F5F5',
+          borderRight: 'none',
+          height: 'calc(100vh - 64px)'
+        }
+      }}
+    >
+      <SideBarContent />
+
+    </Drawer>
   )
 }
 
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import SideBar from "../components/SideBar";
 
 
 
 const Main = () => {
+
+    const [openDrawer, setOpenDrawer] = useState(true);
+    
+    const toggleDrawer = () => {
+        setOpenDrawer(prevState => !prevState);
+    } 
+
     return(
         <div>
-            <Header />
-            <SideBar />
+            <Header toggleDrawer={toggleDrawer} />
+            <SideBar openDrawer={openDrawer} />
             <div>Display mails</div>
         </div>
     )
